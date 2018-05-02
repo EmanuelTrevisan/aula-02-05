1554c96cf596aabb66bb86f387e0058f833c8341
Emanuel O. Trevisan <emanuel.otokovieski@gmail.com>
2018-05-02 11:14:17 -0400

    Alterações Aula 7 - Projeto

--- Aula_02-05/src/br/edu/ifro/AlunoController.java
+++ Aula_02-05/src/br/edu/ifro/AlunoController.java
@@ -5,9 +5,16 @@
  */
 package br.edu.ifro;
 
+import br.edu.ifro.modelo.Aluno;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  * FXML Controller class
@@ -16,6 +23,9 @@ import javafx.fxml.Initializable;
  */
 public class AlunoController implements Initializable {
 
+    @FXML
+    private JFXTextField txtNome;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +33,23 @@ public class AlunoController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void Salvar(ActionEvent event) {
+        EntityManagerFactory enf = Persistence.createEntityManagerFactory("aula");
+        EntityManager en = enf.createEntityManager();
+        
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome(txtNome.getText());
+        
+        en.getTransaction().begin();
+         en.getTransaction().commit();
+        
+        
+    }
+
+    @FXML
+    private void Fechar(ActionEvent event) {
+    }
     
 }
